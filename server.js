363f7e99275341546c831c19218640f0e44cd9c8
sydtinki <43537309+sydtinki@users.noreply.github.com>
2023-12-03 19:17:35 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/weather', async (req, res) => {
+  const { location } = req.body;
+
+  try {
+    const apiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+    const response = await axios.get(apiUrl);
+    const weatherData = response.data;
+
+    res.json({
+      temperature: weatherData.main.temp,
+      description: weatherData.weather[0].description,
+      // Add more relevant data based on the OpenWeatherMap API response
+    });
+  } catch (error) {
+    res.status(400).json({ error: 'Location not found' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
